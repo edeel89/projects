1ca6bf36e67293d69414c8fd57f37702a7ea9607
edeel89 <60333861+edeel89@users.noreply.github.com>
2020-01-27 11:48:26 -0500

    Validating Credit Card Number

--- Validating-Credit-Card.py
+++ Validating-Credit-Card.py
@@ -1,11 +1,17 @@
+#Importing regular expression
 import re
 
+#User to input card number
 card_number = input("Please enter your card number: ")
-nv = "Invalid"
+
+#Condition to check the first number either 4, 5, or 6 and card must be 16 digits
 condition1 = bool(re.match(r"^[456]\d{15}$", card_number))
+#Condition to check the first number also with the hyphen 
 condition2 = bool(re.match(r"^[456]\d{3}\-\d{4}\-\d{4}\-\d{4}$", card_number))
+#Replacing the "-" hyphen
 card_number = card_number.replace("-", "")
 condition3 = bool(re.match(r"(?!.*(\d)(-?\1){3})", card_number))
+#If condition 1, 2, or 3 is true then print valid, if not print invalid
 if (condition1 or condition2) and condition3:
     v = "You card number is Valid"
     print (v)
